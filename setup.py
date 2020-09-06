b8a0ebfc9285ae53751804b32c21285cecd87385
Junxiang Wang <xianggebenben@163.com>
2020-09-06 17:44:19 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,2 @@
+from distutils.core import setup
+setup(name="pdADMM", version="0.0.1", description="parallel deep learning Alternating Direction Method of Multipliers", author="Junxiang Wang", py_modules=['pdADMM.common','pdADMM.input_data'])
